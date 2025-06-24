09f1a7a12d734cb789f6eb6107cce6a10c8d42d1
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-06-24 18:24:04 +0200

    css

--- src/components/Rewards.tsx
+++ src/components/Rewards.tsx
@@ -4,15 +4,18 @@ export default function Rewards() {
       <div className="text-yellow-400 font-bold">
         Rewards (for each leaderboard)
       </div>
-      <div className="flex flex-row items-center justify-center gap-2 mb-2">
-        <p className="flex flex-row no-wrap items-baseline">
-          <span className="text-2xl">🥇</span> 300 EURe
+      <div className="flex flex-row items-center justify-center gap-2 mb-2 font-bold text-left">
+        <p className="flex flex-row no-wrap items-end">
+          <span className="text-2xl">🥇</span>
+          <span className="pb-1">300 EURe</span>
         </p>
-        <p className="flex flex-row no-wrap items-baseline">
-          <span className="text-2xl">🥈</span> 200 EURe
+        <p className="flex flex-row no-wrap items-end">
+          <span className="text-2xl">🥈</span>
+          <span className="pb-1">200 EURe</span>
         </p>
-        <p className="flex flex-row no-wrap items-baseline">
-          <span className="text-2xl">🥉</span> 100 EURe
+        <p className="flex flex-row no-wrap items-end">
+          <span className="text-2xl">🥉</span>
+          <span className="pb-1">100 EURe</span>
         </p>
       </div>
       <div>#4-#10 50 EURe + T-shirt</div>
